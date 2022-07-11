68af20e3781990feaa8045b960b7b2bc2921b689
Daniel <daniel391992@gmail.com>
2022-07-10 20:51:56 -0500

    app service

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        if(app()->enviroment('production')) {
+        if(app()->environment('production')) {
             URL::forceScheme('https');
          }
     }
